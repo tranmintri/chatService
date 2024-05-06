db9e35a44b036adc71b766030d323a24fcfdf513
tranminhtri <53221375+tranmintri@users.noreply.github.com>
2024-05-06 10:13:03 +0700

    clone again

--- client/src/constants/QueryKey.js
+++
@@ -1,5 +0,0 @@
-const GET_USER_INFO = "GET_USER_INFO";
-
-export default {
-  GET_USER_INFO,
-};
--- client/src/utils/config.js
+++
@@ -1,2 +0,0 @@
-export const appId = 1579457023;
-export const ServerSecret = "bb64d83d57ae52b332d5f063ea134ef8";
